07cec970c2cffc2ed32aad51e28c54b14774e57e
jainHarsh03 <harshjain0325@gmail.com>
2025-06-24 13:29:11 +0530

    updated files

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(cors())
 app.use(express.json())
-app.use(express.static("public"))
+app.use(express.static("../public"))
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/drill-tracker", {
@@ -35,11 +35,11 @@ app.use("/api/users", userRoutes)
 
 // Serve static files
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"))
+  res.sendFile(path.join(__dirname, "../public", "login.html"))
 })
 
 app.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "dashboard.html"))
+  res.sendFile(path.join(__dirname, "../public", "dashboard.html"))
 })
 
 app.listen(PORT, () => {
---
+++ start-server.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+/**
+ * Start script for the Drill Tracker backend server
+ */
+
+require('./src/server.js');
